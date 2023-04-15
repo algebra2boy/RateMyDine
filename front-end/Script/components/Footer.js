3bb7bb39ecf8bc3b2ef1f444862878ed84f0e28a
Yongye Tan <yongyetan1209@gmail.com>
2023-04-15 15:14:55 -0400

    Upgrade: Modularize HTML to components

---
+++ front-end/Script/components/Footer.js
@@ -0,0 +1,30 @@
+// define a footer class
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+        <footer>
+            <div class="container-fluid text-center text-lg-start" style="background-color: maroon">
+              <div class="footer">
+                <center>
+                  <img src="Pictures/logo.png" id="footer-logo" alt="logo" class="img-fluid">
+                </center>
+              </div>
+            </div>
+
+
+            <!-- Copyright -->
+            <div>
+              <div class="container-fluid text-center text-white p-3" style="background-color: maroon">
+                © 2023-2024 RateMyDine All rights reserved.
+              </div>
+            </div>
+        </footer>
+        `;
+    }
+}
+
+customElements.define('footer-component', Footer);
