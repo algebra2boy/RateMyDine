0afcb4ec9e6e6ef5e93abeeb1547bb4f63b21023
Yongye Tan <yongyetan1209@gmail.com>
2023-04-15 11:27:10 -0400

    Bugfix: fix time format

--- front-end/Script/time.js
+++ front-end/Script/time.js
@@ -1,11 +1,19 @@
+import { DiningHallList } from "./diningHall.js"
 function currentTime() {
     let todayDate = new Date();
     let dayzone = "AM"
     if (todayDate.getHours() > 12) {
         dayzone = "PM";
     }
-    let time = todayDate.getHours() + ":" + todayDate.getMinutes() + ":" + todayDate.getSeconds() + " " + dayzone;
-    document.getElementById("search-box").placeholder = "Rating My Dining Hall At " + time;
+
+    let hour = todayDate.getHours(), minute = todayDate.getMinutes(), second = todayDate.getSeconds();
+    // format the hour 
+    let hh = (String(hour).length === 1) ? "0" + hour : hour;
+    let mm = (String(minute).length === 1) ? "0" + minute : minute;
+    let ss = (String(second).length === 1) ? "0" + second : second;
+
+    let time = hh + ":" + mm + ":" + ss + " " + dayzone;
+    document.getElementById("search-box").placeholder = "Scroll to rate at " + time;
     // call the function every 1000ms, which is 1 second
     var t = setTimeout(currentTime, 1000);
 }
