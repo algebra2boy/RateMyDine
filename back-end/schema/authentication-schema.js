672e90c4ad43d505118bf5e2e0a38df3071b46d5
Yongye Tan <yongyetan1209@gmail.com>
2023-04-18 10:21:56 -0400

    Refacor the login and sign up schema to one file

--- back-end/schema/authentication-schema.js
+++ back-end/schema/authentication-schema.js
@@ -8,4 +8,9 @@ const ValidateSignupSchema = [
     body("signUpPassword", "Please enter a password at least 8 character",).isLength({ min: 8 }),
 ]
 
-export { ValidateSignupSchema };
+const ValidateLoginSchema = [
+    body('LoginEmail', "Email is not valid").notEmpty().isEmail(),
+    body("LoinInPassword", "Please enter a password at least 8 character",).isLength({ min: 8 }),
+]
+
+export { ValidateSignupSchema, ValidateLoginSchema };
--- back-end/schema/login-schema.js
+++
@@ -1,8 +0,0 @@
-import { body } from 'express-validator';
-
-const ValidateLoginSchema = [
-    body('LoginEmail', "Email is not valid").notEmpty().isEmail(),
-    body("LoinInPassword", "Please enter a password at least 8 character",).isLength({ min: 8 }),
-]
-
-export { ValidateLoginSchema };
