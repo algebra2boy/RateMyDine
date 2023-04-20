ff2c3bc8fd170933c038b3dc55d54b0483fbad46
Ivan Liu <ivanliu@umass.edu>
2023-04-19 21:13:23 -0400

    Modified Review Card

--- back-end/date.js
+++ back-end/date.js
@@ -2,7 +2,7 @@ function currentDate() {
     let todayDate = new Date();
     document.getElementById("time").innerHTML =  `Date: ${todayDate.toLocaleDateString()}`;
     // call the function every 3600000ms, which is 1 hr
-    var t = setTimeout(currentTime, 3600000);
+    var t = setTimeout(currentDate, 3600000);
 }
 
 currentDate();
