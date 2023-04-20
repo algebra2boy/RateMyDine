457ee39171fab4fe543bda6a39b0394d6c1f5bf5
Yongye Tan <yongyetan1209@gmail.com>
2023-04-20 19:11:26 -0400

    Successfully create user using pouchDB at /signup

--- back-end/server/Routers/authRouters.js
+++ back-end/server/Routers/authRouters.js
@@ -3,18 +3,17 @@ import path from "path"; // to find the current path of this project
 import bcrypt from "bcrypt"; // help us hash passwords (for later)
 import { validationResult } from "express-validator";
 import { ValidateSignupSchema, ValidateLoginSchema } from "../../schema/authentication-schema.js"; // used to validate user's input on the login and sign up page
+import { userDB } from "../server.js";
 const authRouter = express.Router();
 
 const __dirname = path.resolve();
-
-// TODO: implement pounchDB and initialize the DB here
 
 console.log(__dirname);
 
 // Express routing documentation: https://expressjs.com/en/guide/routing.html
 // the default endpoint to retrieve main page
 authRouter.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "", "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 })
 
 // signup endpoint to retrieve sign up page
@@ -23,21 +22,34 @@ authRouter.get('/signup', (req, res) => {
 })
 
 // signup for submitting a form
-authRouter.post('/signup', ValidateSignupSchema, (req, res) => {
+authRouter.post('/signup', ValidateSignupSchema, async (req, res) => {
     // user is not following the rules
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        res.json({
+        res.status(500).send({
             message: errors,
-            status: "failure"
+            status: "failure",
         });
         return;
     }
     // TODO: implement the sign up feature when the user input are correct
     // Check if the user is already exists in the DB. If not, then create an row; otherwise, display error teling users that account existed
-    res.json({
-        status: "success"
-    });
+    const { firstName, lastName, userName , SignUpEmail, signUpPassword} = req.body;
+    try {
+        const newRateMyDineUser = await userDB.post({
+            firstName: firstName,
+            lastName : lastName,
+            userName: userName,
+            email: SignUpEmail,
+            password: signUpPassword
+        });
+        res.status(201).send({
+            message: `successfully created user with ID ${newRateMyDineUser.id}`,
+            status: "success"
+        });
+    } catch (error) {
+        res.status(500).send({status: "failure"});
+    }
 });
 
 // login endpoint to retrieve login page
