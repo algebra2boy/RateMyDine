c04caceda9987d23b72b6412b5055066ce477eec
Yongye Tan <yongyetan1209@gmail.com>
2023-04-21 09:24:55 -0400

    Add important comments on reviewRouter

--- back-end/server/Routers/reviewRouters.js
+++ back-end/server/Routers/reviewRouters.js
@@ -3,9 +3,17 @@ import express from "express";
 
 const reviewRouter = express.Router();
 
-// get all the food review from a particular dining hall
+/*
+We will use fetch API to fetch all the food review from a particular dining hall
+- Before this step, checking whether the user is login is important but we can hold off this for now
+    - if user is not login, a json message with a status code 401 (unauthenticated)
+1. we need to check if diningHall name is valid, hence blocking their calls when it is not valid
+2. we need to retrieve food review from this dining hall
+    - if successful, then send a json message with a status code 200 (okay)
+    - if not, send a json message with warning and a status code 404 (Not found)
+*/
 reviewRouter.get("/review/:dininghall", (req, res) => {
-    res.json({"mes": "HELLO"})
+    
 })
 
 // create a new food review for a particular dining hall
