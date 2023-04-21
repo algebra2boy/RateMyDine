e0bdaa3884f8707bfba17ec2f7626d28bd49fbe3
Adam Trac <atrac@umass.edu>
2023-04-21 18:13:04 -0400

    blueprint for backend dininghall

---
+++ back-end/components/diningHall.js
@@ -0,0 +1,9 @@
+//redirect to dining hall page + load information on dining hall + handle review loading
+
+
+function redirect(){
+    //data from arg goes here
+    return function(){
+        
+    }
+}
