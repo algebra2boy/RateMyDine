40b2eb12ee7e23af2e473fd68da9d95f1968f20d
Jicheng Li <jjl01332@gmail.com>
2023-04-22 23:18:55 -0400

    feat: finished routing, needs cosmetic fixes

--- back-end/server/Routers/reviewRouters.js
+++ back-end/server/Routers/reviewRouters.js
@@ -20,32 +20,38 @@ We will use fetch API to fetch all the food review from a particular dining hall
 */
 
 // get all the food review from a particular dining hall
-reviewRouter.get("/review/:dininghall", (req, res) => {
+reviewRouter.get("/review/:dininghall", async (req, res) => {
     let dine = req.params.dininghall;
-    dbUtils.getDoc(dine).then((doc) =>{
-        res.send(doc);
-    })
-   
+    let doc = await dbUtils.getReview(dine);
+    res.send(doc);
 })
 reviewRouter.get("/review/:userID", (req,res) => {
     
 });
 // create a new food review for a particular dining hall
 reviewRouter.post("/review/:diningHall", async (req, res) => {
-    let dine = req.body;
+    let rev = req.body.review;
     let hall = req.params.diningHall;
-    let doc = dbUtils.getDoc(hall);
-})
+    let result = await dbUtils.createReview(hall, rev);
+    res.send(result);
+});
 
 // update an existing food review for a particular dining hall
-reviewRouter.post("/review/:dininghall/:reviewID", (req, res) => {
-
-})
+reviewRouter.post("/review/:dininghall/:reviewID", async (req, res) => {
+    let rev = req.body.review;
+    let hall = req.params.dininghall;
+    let id = req.params.reviewID;
+    let result = await dbUtils.updateReview(hall, rev, id);
+    res.send(result);
+});
 
 // delete an existing food review for a particular dining hall
-reviewRouter.delete("/review/:dininghall/:reviewID", (req, res) => {
-    
-})
+reviewRouter.delete("/review/:dininghall/:reviewID", async (req, res) => {
+    let hall = req.params.diningHall;
+    let id = req.params.reviewID;
+    let result = await dbUtils.deleteReview(hall, id);
+    res.send(result);
+});
 
 reviewRouter.get("/:diningHall", (req, res) => {
     res.sendFile("./front-end/HTML/dining.html", {root: "./"});
@@ -56,6 +62,11 @@ reviewRouter.get("/info/:diningHall", (req, res) => {
     let list = diningInfo.filter((obj) => obj.name.toLowerCase() === hall.toLowerCase());
     if(list.length === 0) res.status(404).send("Not Found");
     else res.send(JSON.stringify(list[0]));
+});
+
+reviewRouter.post("/allReviews", async (req, res) => {
+    let msg = await dbUtils.init();
+    res.send(msg);
 });
 
 export default reviewRouter;
--- back-end/server/reviewDBUtils.js
+++ back-end/server/reviewDBUtils.js
@@ -1,24 +1,41 @@
 import { reviewDB } from "./server.js";
 import { diningReview } from "../MockData/reviews.js";
-async function createDoc(info){
+import { diningInfo } from "../MockData/diningHallInfo.js";
+import { Review } from "../MockData/classDefinitions.js";
+
+const keyMap = {
+    ReviewDescription: "description",
+    FoodQuality: "foodQuality",
+    CustomerService: "customerService",
+    Atmosphere: "atmosphere",
+    Healthiness : "healthiness",
+    SeatAvailability: "seatAvailability",
+    Taste: "taste",
+}
+
+async function createReview(hall, revi){
     try{
-        console.log(info);
-        console.log(JSON.parse(info.Reviews));
-        let res = await reviewDB.put({
-            _id: info.DiningName,
-            Reviews: JSON.parse(info.Reviews),
-        })
+        let doc = await reviewDB.get(hall);
+        let review = JSON.parse(revi);
+        let sum = 100 * ((Number(review["FoodQuality"])/5) + (Number(review["CustomerService"])/5)
+                        + (Number(review["Atmosphere"])/5) + (Number(review["Healthiness"])/5) 
+                        + (Number(review["SeatAvailability"])/5) + (Number(review["Taste"])/5));
+        let average = Math.ceil(sum / 6)  * 0.01 * 5; 
+        let nRev = new Review(""+(Number(doc.reviews[0].id)+1), "April 22, 2023", "ABCDED", average, review.ReviewDescription, 
+        review.FoodQuality,review.Atmosphere, review.Healthiness, review.SeatAvailability, review.Taste);
+        doc.reviews.unshift(nRev);
+        let res = await reviewDB.put(doc);
         return JSON.stringify(res);
     }
     catch (e){
         console.error(e);
     }
 }
-async function getDoc(info){
+async function getReview(info){
     try{
-        console.log(info);
+        // console.log(info);
         let res = await reviewDB.get(info)
-        console.log(JSON.stringify(res));
+        // console.log(JSON.stringify(res));
         console.log(res);
         return JSON.stringify(res);
     }
@@ -26,18 +43,63 @@ async function getDoc(info){
         console.error(e);
     }
 }
-async function updateDoc(info){
-    
+async function updateReview(hall, review, id){
+    try{
+        let doc = await reviewDB.get(hall);
+        for(let i = 0; i < doc["reviews"].length; i++){
+            if(doc.reviews[i].id === id){
+                for(let key in review){
+                    let mapKey = keyMap[key];
+                    doc.reviews[i][mapKey] = review.key;
+                }
+                let res = await reviewDB.put(doc);
+                console.log(res);
+                return true;
+            }
+        }
+        return false;
+    }
+    catch (e) {
+        console.error(e);
+    }
 }
-async function deleteDoc(info){
-    
+async function deleteReview(hall,id){
+    let doc = await reviewDB.get(hall);
+    let found = false;
+    let i = undefined;
+    for(i = 0; i < doc.Reviews.length; i++){
+        if(doc.Reviews[i].id === id){
+            found = true;
+            break;
+        }
+    }
+    if(found){
+        doc.Reviews.splice(i, 1);
+        reviewDB.put(doc);
+    }
+    return found;
 }
 
+async function init(){
+    try{
+        for(let i = 0; i < diningReview.length; i++){
+            await reviewDB.put({
+                _id: diningReview[i].DiningName,
+                reviews: diningReview[i].Reviews,
+            })
+        }
+        return("Successful Initialization");
+    }
+    catch (e){
+        console.error(e);
+    }
+}
 
 
 export {
-    createDoc,
-    getDoc,
-    updateDoc,
-    deleteDoc
+    createReview,
+    getReview,
+    updateReview,
+    deleteReview,
+    init
 }
