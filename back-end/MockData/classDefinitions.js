ddab6764a77352a9286f5ae827060cb73f620ff9
Yongye Tan <yongyetan1209@gmail.com>
2023-04-25 21:14:51 -0400

    Fix names in class definitions

--- back-end/MockData/classDefinitions.js
+++ back-end/MockData/classDefinitions.js
@@ -1,26 +1,26 @@
 class DiningHall {
-    constructor(n, adr, pn, nr, desc){
-        this.name = n;
-        this.address = adr;
-        this.phone = pn;
-        this.reviewCount = nr;
-        this.description = desc;
+    constructor(name, address, phone, reviewCount, description) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.reviewCount = reviewCount;
+        this.description = description;
     }
 }
 
 class Review {
-    constructor(revId, pt, user, stars, desc, fq, custServ, atmo, health, seats, t){
-        this.id = revId;
-        this.postTime = pt;
-        this.userId = user;
-        this.overall = stars;
-        this.description = desc;
-        this.foodQuality = fq;
-        this.customerService = custServ;
-        this.atmosphere = atmo;
-        this.healthiness = health;
-        this.seatAvailability = seats;
-        this.taste = t;
+    constructor(reviewID, postTime, userID, overAllStars, description, foodQuality, customerService, atmosphere, healthiness, seatAvailability, taste) {
+        this.id = reviewID;
+        this.postTime = postTime;
+        this.userId = userID;
+        this.overall = overAllStars;
+        this.description = description;
+        this.foodQuality = foodQuality;
+        this.customerService = customerService;
+        this.atmosphere = atmosphere;
+        this.healthiness = healthiness;
+        this.seatAvailability = seatAvailability;
+        this.taste = taste;
     }
 }
 
