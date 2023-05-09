319f42b229f9076263dbba501f34796f251b9113
Yongye Tan <yongyetan1209@gmail.com>
2023-05-09 15:15:43 -0400

    load dining page describing using db

--- server/Routers/reviewRouters.js
+++ server/Routers/reviewRouters.js
@@ -1,6 +1,5 @@
 import express from "express";
 import * as dbUtils from "../DataBase/reviewDBUtils.js"; // helper for database CRUD
-import { diningInfo } from "../MockData/diningHallInfo.js"; // every dining hall review data
 import server from "../server.js";
 
 const reviewRouter = express.Router();
@@ -11,19 +10,26 @@ reviewRouter.get("/diningInfo", async (req, res) => {
     res.send(diningInfo);
 });
 
-
+// render the dining hall page
 reviewRouter.get("/:diningHall", (req, res) => {
     res.sendFile("./client/HTML/dining.html", { root: "./" });
 });
 
-reviewRouter.get("/info/:diningHall", (req, res) => {
+// retrieve dining info 
+reviewRouter.get("/info/:diningHall", async (req, res) => {
     // grabs the dining hall name from the URL
-    let hall = req.params.diningHall;
-    let list = diningInfo.filter((obj) => obj.name.toLowerCase() === hall.toLowerCase()); //filters through the object imported from diningHallInfo.js
-    
-    if (list.length === 0) res.status(404).send("Not Found"); //if length is 0 return 404 not Found as Dining Hall information doesn't exist
-    else res.send(JSON.stringify(list[0])); // else return the first element -> should be the only 1 in the array, which is the info for the dining hall.
+    let name = req.params.diningHall;
+    const diningInfo = await server.diningInfo.findOne({"name": name});
 
+    // Dining Hall information doesn't exist
+    if (diningInfo === null) {
+        res.status(404).send({
+            "message": `${hall} not found in the database`,
+            "status": "failure"
+        });
+    } else {
+        res.send(diningInfo);
+    }
 });
 
 // get all the food review from a particular dining hall
@@ -55,11 +61,6 @@ reviewRouter.post("/review/:dininghall/:reviewID", async (req, res) => {
     res.send(result);
 });
 
-// // fill the database with the Mockdata for testing and rendering purposes
-// reviewRouter.post("/allReviews", async (req, res) => { 
-//     let msg = await dbUtils.init();
-//     res.send(msg);
-// });
 
 // delete an existing food review for a particular dining hall
 reviewRouter.delete("/review/:dininghall/:reviewID", async (req, res) => {
