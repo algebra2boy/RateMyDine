3e9e49071db8b0243f881d81c7881b59d74f121a
Adam Trac <atrac@umass.edu>
2023-05-16 18:55:28 -0400

    minor cosmetic changes

--- client/components/profile.js
+++ client/components/profile.js
@@ -6,22 +6,8 @@ edit_info = edit_info.map((element) => (document.getElementById(element)));
 info      = info.map((element) => (document.getElementById(element))); // before editing the info
 buttons   = buttons.map((elememt) => (document.getElementById(elememt)));
 
-document.getElementById('edit-info').addEventListener('click', () => {
-    info.forEach((element) => element.style.display = "none");
-    // bring user's info to the text input field
-    edit_info.forEach((element, index) => {  element.type  = "text"; element.value = info[index].innerHTML; } );
-    // make button visible if user wants to edit info
-    buttons.forEach((button) => button.style.visibility = "visible");
-});
 
-// cancel button
-buttons[0].addEventListener('click', () => {
-    info.map((element)      => element.removeAttribute("style") );
-    edit_info.map((element) => element.type = "hidden");
-    buttons.map((element)   => element.style.visibility = "hidden");
-});
-
-// below is where the profile page is being render
+// below is where the profile page is being rendered
 async function renderProfile() {
     const name = window.location.href.split("/")[4];
     try {
@@ -56,11 +42,29 @@ async function updateProfile() {
     }
 }
 
+document.getElementById('edit-info').addEventListener('click', () => {
+    info.forEach((element) => element.style.display = "none");
+    // bring user's info to the text input field
+    edit_info.forEach((element, index) => {  element.type  = "text"; element.value = info[index].innerHTML; } );
+    // make button visible if user wants to edit info
+    buttons.forEach((button) => button.style.visibility = "visible");
+    edit_info.forEach((x) => x.style.backgroundColor = "rgb(248, 248, 248)");
+});
+
+// cancel button
+buttons[0].addEventListener('click', () => {
+    info.map((element)      => element.removeAttribute("style") );
+    edit_info.map((element) => element.type = "hidden");
+    buttons.map((element)   => element.style.visibility = "hidden");
+    edit_info.forEach((x) => x.style.backgroundColor = "rgb(255, 255, 255)");
+});
+
 // save button 
 buttons[1].addEventListener('click', async() => {
     info.map((element, index)   => { element.removeAttribute("style"); element.innerHTML = edit_info[index].value } );
     edit_info.forEach((element) => element.type  = "hidden" );
     buttons.forEach((button)    => button.style.visibility = "hidden");
+    edit_info.forEach((x) => x.style.backgroundColor = "rgb(255, 255, 255)");
     await updateProfile();
 });
 
