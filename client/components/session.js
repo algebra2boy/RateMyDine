8c0c80daf184cb7343bbeca86ba05ce3fd260cca
Yongye Tan <yongyetan1209@gmail.com>
2023-05-19 18:23:12 -0400

    fix session inconsistency after closing the browser

--- client/components/session.js
+++ client/components/session.js
@@ -7,50 +7,48 @@ const signup_btn = document.getElementById("signup-btn");
 // this session variable will be gone after the user clicks on the logout or session is inactive after 15 minutes
 // isAuthenticated is either true or false
 
+// reset the button to the "log-in" state
+function resetToDefault() {
+    sessionStorage.setItem("isAuthenticated", JSON.stringify(false));
+    login_btn.innerHTML  = "Log in";
+    signup_btn.innerHTML = "Sign up";
+    login_btn.href       = "/login";
+    signup_btn.href      = "/signup";
+}
+
+function destorySession(event) {
+    event.preventDefault();
+    const options = { headers: { "Content-Type": "application/json" }, method: "POST" };
+    fetch("http://localhost:3000/logout", options)
+        .then((request) => {
+            if (request.redirected) {
+                console.log("session has been destoryed remotely and on local");
+                resetToDefault();
+
+                // deactivate click after switching back to "login" mode
+                signup_btn.removeEventListener('click', destorySession);
+
+                // redirect to the main page
+                window.location.replace(request.url);
+                location.reload();
+            }
+        }).catch(error => {
+            console.error(error);
+        });
+}
+
 // send a request to server to see if there is any active session
 fetch("http://localhost:3000/session")
     .then((sessionResponse => sessionResponse.json()))
     .then((session) => {
-    // remote session is active, therefore turn on the session even though browser is closed
+        // remote session is active, therefore turn on the session even though browser is closed
         if ("passport" in session) {
-            sessionStorage.setItem("isAuthenticated", JSON.stringify(true)); 
-            login_btn.innerHTML     = "Profile"
-            signup_btn.innerHTML    = "Log out"
-            login_btn.href          = "/profile";
-            signup_btn.href         = "/logout";
+            sessionStorage.setItem("isAuthenticated", JSON.stringify(true));
+            login_btn.innerHTML  = "Profile";
+            signup_btn.innerHTML = "Log out";
+            login_btn.href       = "/profile";
+            signup_btn.removeAttribute("href"); // no need to add /logout since we manually do a post logout on the endpoint
+            signup_btn.style.cursor = "pointer";
+            signup_btn.addEventListener("click", destorySession);
         }
 });
-
-// reset the button to the "log-in" state
-function resetToDefault() {
-    login_btn.innerHTML     = "Log in";
-    signup_btn.innerHTML    = "Sign up";
-    login_btn.href          = "/login";
-    signup_btn.href         = "/signup";
-}
-
-// user is authenticated
-if (JSON.parse(sessionStorage.getItem("isAuthenticated"))) {
-
-    signup_btn.addEventListener("click", destorySession);
-
-    function destorySession(event) {
-        event.preventDefault();
-        const options = { headers: { "Content-Type": "application/json" }, method: "POST" };
-        fetch("http://localhost:3000/logout", options)
-            .then((request) => {
-                if (request.redirected) {
-                    sessionStorage.setItem("isAuthenticated", JSON.stringify(false));
-                    console.log("session has been destoryed remotely and on local");
-                    resetToDefault();
-                    // deactivate click after switching back to "login" mode
-                    signup_btn.removeEventListener('click', destorySession);
-
-                    // redirect to the main page
-                    window.location.replace(request.url);
-                }
-            }).catch(error => {
-                console.error(error);
-            });
-    }
-}
